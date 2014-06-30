ee5950163da78e57db59dbd94687349f929e31eb
shanchuan_3180 <shanchaun_3180@163.com>
2014-06-30 17:54:19 +0800

    dd

---
+++ src/org/jack/app/webservice/HelloTest.java
@@ -0,0 +1,5 @@
+package org.jack.app.webservice;
+
+public class HelloTest {
+
+}
---
+++ src/org/jack/app/webservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.app.jack.org/")
+package org.jack.app.webservice;
